a4c61cef6c4579225c2f2bb607039c40302bb0e7
Matthew Gilliam <chase.gilliam@gmail.com>
2026-06-01 12:58:21 -0400

    feat: add MaxMind GeoLite2 attribution to audit log page

--- test/hexpm_web/controllers/dashboard/audit_log_controller_test.exs
+++ test/hexpm_web/controllers/dashboard/audit_log_controller_test.exs
@@ -18,6 +18,18 @@ defmodule HexpmWeb.Dashboard.AuditLogControllerTest do
       assert html_response(conn, :ok) =~ "Recent Activities"
     end
 
+    test "shows MaxMind attribution for location data" do
+      user = insert(:user)
+
+      response =
+        build_conn()
+        |> test_login(user)
+        |> get("/dashboard/audit-logs")
+        |> html_response(:ok)
+
+      assert response =~ "MaxMind"
+    end
+
     test "shows the most recent audit logs for current user" do
       user = insert(:user)
 
